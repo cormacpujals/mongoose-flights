011ba67415c7e6bcbda7ed254e7b7b2dc0eeccb5
Cormac Pujals <cmpujals@gmail.com>
2022-10-27 22:04:17 -0700

    adds create/visualize ticket functionality

--- controllers/flights.js
+++ controllers/flights.js
@@ -17,7 +17,6 @@ function index(req, res) {
 function show(req, res) {
   Flight.findById(req.params.id, function(err, flight) {
     Ticket.find({flight: flight._id})
-          // .populate('flight')
           .exec(function (err, tickets) {
             console.log(JSON.stringify(tickets)); // TODO remove after debugging
             res.render('flights/show', {
